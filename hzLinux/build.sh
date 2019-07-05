fb60d9cfe05ef7cfccbf61054720f35c6f28bdb0
HaoZeke <rohit.goswami@aol.com>
2019-07-06 02:01:13 +0530

    build: Use the offline_releng build script

--- hzLinux/build.sh
+++ hzLinux/build.sh
@@ -12,6 +12,7 @@ work_dir=work
 out_dir=out
 gpg_key=
 
+offline_mirror_path=//x86_64/airootfs/srv/http/arch_offline
 verbose=""
 script_path=$(readlink -f ${0%/*})
 
@@ -220,6 +221,103 @@ make_prepare() {
 }
 
 # Build ISO
+build_aur () {
+    # TODO: Create a temp dir that isn't just the package name,
+    #       just in case there's a freak accidental name collision
+    old_dir=`pwd`
+    package=$1
+
+    # Extract the AUR package.
+    cd /tmp
+    rm -rf ${package} ${package}.tar.gz
+    wget "https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz"
+    tar xvzf "${package}.tar.gz"
+
+    # Prep with a build-user (removed at the end):
+    # TODO: Check if already exists, if so, randomize name/don't remove at the end.
+    # TODO: Don't give permission to wheel, give it only to this user (easy, but needs debugging first)
+    EXISTS=1
+    if [ ! $(id -u builder) ]; then
+        EXISTS=0
+        useradd -m -G wheel builder
+        sed -i 's/# %wheel ALL=(ALL) NO/%wheel ALL=(ALL) NO/' /etc/sudoers
+    fi
+    cd ${package}
+    build_dir=$(pwd)
+    chown -R builder.builder /tmp/${package}
+    echo " => Buiilding ${package}"
+    # 2>&1
+    su - builder -c "(cd ${build_dir}; makepkg -s --noconfirm)" >/dev/null
+    sed -i 's/%wheel ALL=(ALL) NO/# %wheel ALL=(ALL) NO/' /etc/sudoers
+
+    echo " => Adding ${package} to local AUR hosting directory ${offline_mirror_path}"
+    mkdir -p "${offline_mirror_path}/os/x86_64"
+    sh -c "cp *.xz ${offline_mirror_path}/os/x86_64"
+    sh -c "repo-add ${offline_mirror_path}/os/x86_64/arch_offline.db.tar.gz ${offline_mirror_path}/os/x86_64/*.xz"
+
+    ## Long term storage inside the ISO? (if we want to install from CD to disk or host it to others)
+    # sh -c "mv *.xz ${old_dir}/$2/$1.pkg.tar.xz"
+
+    cd ${old_dir}
+    if [ ${EXISTS} -lt 1 ]; then
+        userdel builder
+        rm -rf /home/builder
+    fi
+    rm -rf /tmp/${package}
+    rm /tmp/${package}.tar.gz
+}
+
+make_aur_packages() {
+    touch ${script_path}/packages.aur
+    packages=$(cat ${script_path}/packages.aur)
+    if [[ -n ${packages} ]]; then
+        for package in ${packages}; do
+            build_aur ${package}
+        done
+    fi
+}
+
+make_offline_mirror() {
+    packages=$(cat ${script_path}/packages.x86_64)
+    if  [[ -n ${packages} ]]; then
+        echo " => Downloading packages to offline mirror."
+        sh -c "pacman --noconfirm --dbpath /tmp/ -Syu -w --cachedir ${offline_mirror_path} $(echo ${packages} | sed ':a;N;$!ba;s/\n/ /g')"
+    fi
+    echo " => Downloading core packages to offline mirror."
+    #sh -c "pacman --noconfirm --dbpath /tmp/ -Syu -w --cachedir ${offline_mirror_path} gawk gettext grep gzip pacman sed texinfo util-linux"
+    sh -c "pacman --noconfirm --dbpath /tmp/ -Syu -w --cachedir ${offline_mirror_path} base base-devel"
+    sh -c "repo-add ${offline_mirror_path}/arch_offline.db.tar.gz ${offline_mirror_path}/*.xz"
+}
+patch_in_local_mirror() {
+    # [arch_offline]\n
+    # SigLevel = Optional TrustAll\n
+    # Server = file:///tmp/archlive_offline/work/x86_64/airootfs/srv/http/arch_offline/os/x86_64/
+    # sh -c "sed -i 's?\[core\]?\[arch_offline\]\nServer = file://${script_path}/${work_dir}/x86_64/airootfs/srv/http/\$repo/os/\$arch\nSigLevel = Optional TrustAll\n\n&?' ${work_dir}/pacman.conf"
+    cat <<EOF >> ${work_dir}/pacman.conf
+[arch_offline]
+SigLevel = Optional TrustAll
+Server = file://${offline_mirror_path}/os/x86_64/
+EOF
+}
+
+install_aur() {
+    mkarchiso ${verbose} -w "${work_dir}/x86_64" -C "${work_dir}/pacman.conf" -D "${install_dir}" -p "$(grep -h -v ^# ${script_path}/packages.aur)" install
+}
+
+finalize_offline() {
+    ## == Remove online repo references
+    sed -i 's?\[core\]?# &?' ${script_path}/${work_dir}/x86_64/airootfs/etc/pacman.conf
+    sed -i 's?\[extra\]?# &?' ${script_path}/${work_dir}/x86_64/airootfs/etc/pacman.conf
+    sed -i 's?\[community\]?# &?' ${script_path}/${work_dir}/x86_64/airootfs/etc/pacman.conf
+    sed -i 's?Include = /etc/pacman.d/mirrorlist?# &?' ${script_path}/${work_dir}/x86_64/airootfs/etc/pacman.conf
+
+    cat <<EOF >> ${script_path}/${work_dir}/x86_64/airootfs/etc/pacman.conf
+[arch_offline]
+SigLevel = Optional TrustAll
+Server = file:///srv/http/arch_offline/os/x86_64/
+EOF
+}
+
 make_iso() {
     mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -P "${iso_publisher}" -A "${iso_application}" -o "${out_dir}" iso "${iso_name}-${iso_version}-x86_64.iso"
 }
@@ -240,7 +338,8 @@ while getopts 'N:V:L:P:A:D:w:o:g:vh' arg; do
         w) work_dir="${OPTARG}" ;;
         o) out_dir="${OPTARG}" ;;
         g) gpg_key="${OPTARG}" ;;
-        v) verbose="-v" ;;
+        v) offline_mirror_path=//x86_64/airootfs/srv/http/arch_offline
+verbose="-v" ;;
         h) _usage 0 ;;
         *)
            echo "Invalid argument '${arg}'"
@@ -255,6 +354,12 @@ run_once make_pacman_conf
 run_once make_basefs
 run_once make_packages
 run_once make_setup_mkinitcpio
+run_once make_aur_packages
+run_once make_offline_mirror
+run_once patch_in_local_mirror
+run_once install_aur
+run_once finalize_offline
+
 run_once make_customize_airootfs
 run_once make_boot
 run_once make_boot_extra
