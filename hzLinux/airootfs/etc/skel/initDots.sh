7051584b6a18d60639b1c344364c5b3528d06af5
Rohit Goswami <rog32@hi.is>
2022-12-21 11:25:00 +0000

    MAINT: Update to archiso releng

--- hzLinux/airootfs/etc/skel/initDots.sh
+++ hzLinux/airootfs/etc/skel/initDots.sh
@@ -2,5 +2,7 @@
 
 mkdir -p $HOME/Git/Github
 git clone https://github.com/HaoZeke/Dotfiles $HOME/Git/Github/Dotfiles
-cd $HOME/Git/Github/Dotfiles
-dotgit restore hzArchiso
+# cd $HOME/Git/Github/Dotfiles
+# dotgit restore hzArchiso
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply HaoZeke --branch chezmoi
+chezmoi init
